ae6cf310b01a50b2faa4a5de53dc197d60b0ad1b
taquero-programador <jrangel93@proton.me>
2024-02-20 17:34:02 -0600

    feat(main, crud): add more endpoints for API.

--- scrap/scrap_api/crud.py
+++ scrap/scrap_api/crud.py
@@ -5,13 +5,17 @@ from models import Inmo
 
 
 # get all inmos
-def get_inmo(db: Session, skip: int=0, limit: int=100):
+def get_inmo(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Inmo).offset(skip).limit(limit).all()
 
 
+def get_all_fracc_by_inmo(db: Session, municipio: str):
+    return db.query(Inmo).filter(Inmo.municipio==municipio).all()
+
+
 def get_inmo_by_fracc(db: Session, municipio: str, fraccionamiento: str):
-    return db.query(Inmo).filter(Inmo.municipio == municipio, inmo.fraccionamiento == fraccionamiento).all()
+    return db.query(Inmo).filter(Inmo.municipio==municipio, Inmo.fraccionamiento==fraccionamiento).all()
 
 
 def get_model(db: Session, municipio: str, fraccionamiento: str, model: str):
-    return db.query(Inmo).filter(Inmo.municipio == municipio, Inmo.fraccionamiento == fraccionamiento, Inmo.model == model).first()
+    return db.query(Inmo).filter(Inmo.municipio==municipio, Inmo.fraccionamiento==fraccionamiento, Inmo.model==model).first()
--- scrap/scrap_api/main.py
+++ scrap/scrap_api/main.py
@@ -17,6 +17,19 @@ def get_db():
         db.close()
 
 
+# return one or more items
 @app.get("/inmuebles/", response_model=list[schemas.Inmo])
-def read_inmos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_inmos(skip: int=0, limit: int=100, db: Session=Depends(get_db)):
     return crud.get_inmo(db, skip=skip, limit=limit)
+
+
+# return all fracc in municipio
+@app.get("/inmuebles/{municipio}", response_model=list[schemas.Inmo])
+def read_fracc_in_inmo(municipio: str, db: Session=Depends(get_db)):
+    return crud.get_all_fracc_by_inmo(db, municipio=municipio)
+
+
+# return fracc in municipio
+@app.get("/inmuebles/{municipio}/{fraccionamiento}", response_model=list[schemas.Inmo])
+def read_inmos_by_fracc(municipio: str, fraccionamiento: str, db: Session=Depends(get_db)):
+    return crud.get_inmo_by_fracc(db, municipio=municipio, fraccionamiento=fraccionamiento)
