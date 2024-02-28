1011d0b1ef34a4e1f00ea1bff61b05b369497d93
taquero-programador <jrangel93@proton.me>
2024-02-28 08:52:29 -0600

    fix: database

--- scrap/scrapi/database.py
+++ scrap/scrapi/database.py
@@ -4,13 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-<<<<<<< HEAD
-SQLALCHEMY_DATABASE_URL = "sqlite:///./inmubles.db"
-=======
-# from config import DATABASE_URL
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./inmo.db"
->>>>>>> af63b0abd5b03787b52af9a08defd7e181d8f948
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
